2219fdf2c2849ce5ff149234a80f83843b302b5e
Paulo Machado <pauloalexandre3d@gmail.com>
2019-08-23 23:33:00 +0100

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import App from './components/App';
+ReactDOM.render(<App />, document.querySelector('#root'));
