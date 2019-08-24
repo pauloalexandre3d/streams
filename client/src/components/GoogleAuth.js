8b36f0be3151c2a332208812e008df9a32f7b964
Paulo Machado <pauloalexandre3d@gmail.com>
2019-08-24 23:35:05 +0100

    feature: Adding OAuth2

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { throwStatement, tsMethodSignature } from '@babel/types';
 
 class GoogleAuth extends React.Component {
   state = { isSignedIn: null };
@@ -14,18 +15,40 @@ class GoogleAuth extends React.Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
-    console.log(this.state.isSignedIn);
     if (this.state.isSignedIn === null) {
-      return <div>I don't know if you are signed in</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in!</div>;
+      return (
+        <button onClick={this.onSignOut} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>I am not signed in</div>;
+      return (
+        <button onClick={this.onSignIn} className="ui red google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
     }
   }
 
