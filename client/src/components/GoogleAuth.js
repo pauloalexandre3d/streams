1ade3b2c34ef703326bcc4c1bd6e2f78b3e9f092
Paulo Machado <pauloalexandre3d@gmail.com>
2019-08-24 23:15:48 +0100

    feature: Adding OAuth2

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client
+        .init({
+          clientId:
+            '451356196111-qbfefn9nnakqu415ft2vmhtp05a56sas.apps.googleusercontent.com',
+          scope: 'email'
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
+    });
+  }
+
+  renderAuthButton() {
+    console.log(this.state.isSignedIn);
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if you are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
