38fc48e401c93c56cf64e2c0e1fcf8a5feaef274
Paulo Machado <pauloalexandre3d@gmail.com>
2019-08-25 23:20:08 +0100

    feature: Connecting reducers and creators

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,14 @@
+const INITIAL_STATE = {
+  isSignIn: null
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'SIGN_IN':
+      return { ...state, isSignedIn: true };
+    case 'SIGN_OUT':
+      return { ...state, isSignedIn: false };
+    default:
+      return state;
+  }
+};
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
+import authReducer from './authReducer';
+
 export default combineReducers({
-  fixme: () => {
-    return 'abobora';
-  }
+  auth: authReducer
 });
